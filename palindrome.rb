278ee88f3597843f3319cf7353fefd9a652f50f1
Fritz Meissner <fritz.meissner@gmail.com>
2018-02-20 13:29:37 +0200

    Handled capitalisation

--- palindrome.rb
+++ palindrome.rb
@@ -2,6 +2,7 @@ def palindrome(arg)
   arg.gsub!("\n", "") # get rid of new line character from input
   arg.lstrip # remove leading space
   arg.rstrip # remove trailing space
+  arg.downcase # simplest way of handling capitalisation
   arg.each_char do |c|
     index = arg.index(c)
     if arg[-index - 1] != c
