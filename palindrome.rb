833849969902449dbb3716fec33f6a157815a5c8
Fritz Meissner <fritz.meissner@gmail.com>
2018-02-20 13:14:06 +0200

    introduced failing specs that I expect to pass

--- palindrome.rb
+++ palindrome.rb
@@ -3,9 +3,11 @@ def palindrome(arg)
   arg.lstrip # remove leading space
   arg.rstrip # remove trailing space
   arg.downcase # simplest way of handling capitalisation
-  arg.each_char do |c|
-    index = arg.index(c)
-    if arg[-index - 1] != c
+  first_half = arg[0..(arg.length / 2 - 1)]
+  second_half = arg[(arg.length / 2)..(arg.length - 1)]
+  # compare first half with second half
+  for i in (0..(arg.length / 2))
+    if second_half[-i - 1] != first_half[i]
       return false 
     end
   end
--- palindrome_spec.rb
+++ palindrome_spec.rb
@@ -20,4 +20,20 @@ describe 'palindrome' do
   it 'returns true for " AbbA "' do
     expect(palindrome(' AbbA ')).to eq(true)
   end
+
+  it 'returns true for "able was i ere i saw elba"' do
+    expect(palindrome('able was i ere i saw elba')).to eq(true)
+  end
+
+  it 'returns true for "aaa"' do
+    expect(palindrome('aaa')).to eq(true)
+  end
+
+  it 'returns true for "aaabbcbbaaa"' do
+    expect(palindrome('aaabbcbbaaa')).to eq(true)
+  end
+
+  it 'returns true for "Aaa"' do
+    expect(palindrome('Aaa')).to eq(true)
+  end
 end
