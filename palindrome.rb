10b29a37dc077178d934987fa6c53ee44b0e6ed1
Fritz Meissner <fritz.meissner@gmail.com>
2018-02-20 13:14:06 +0200

    introduced failing specs that I expect to pass

--- palindrome.rb
+++ palindrome.rb
@@ -2,9 +2,11 @@ def palindrome(arg)
   arg.gsub!("\n", "") # get rid of new line character from input
   arg.lstrip # remove leading space
   arg.rstrip # remove trailing space
-  arg.each_char do |c|
-    index = arg.index(c)
-    if arg[-index - 1] != c
+  first_half = arg[0..(arg.length / 2 - 1)]
+  second_half = arg[(arg.length / 2)..(arg.length - 1)]
+  # compare first half with second half
+  for i in (0..(arg.length / 2))
+    if second_half[-i - 1] != first_half[i]
       return false 
     end
   end
--- palindrome_spec.rb
+++ palindrome_spec.rb
@@ -16,4 +16,20 @@ describe 'palindrome' do
   it 'returns true for " abba "' do
     expect(palindrome(' abba ')).to eq(true)
   end
+
+  it 'returns true for "able was i ere i saw elba"' do
+    expect(palindrome('able was i ere i saw elba')).to eq(true)
+  end
+
+  it 'returns true for "aaa"' do
+    expect(palindrome('aaa')).to eq(true)
+  end
+
+  it 'returns true for "aaabbcbbaaa"' do
+    expect(palindrome('aaabbcbbaaa')).to eq(true)
+  end
+
+  it 'returns true for "Aaa"' do
+    expect(palindrome('Aaa')).to eq(true)
+  end
 end
